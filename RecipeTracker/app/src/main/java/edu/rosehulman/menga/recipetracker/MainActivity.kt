a0e796c66ab7b2bc18f30921b4835e0cebd612ef
Andrew Meng <menga@rose-hulman.edu>
2019-07-12 23:10:52 -0400

    Some structure for main page
    Buttons added in square arrangement. Very likely that this will need reformatted - only know that it works on the only phone on which it's been tested. Clearly, listeners need changed.

--- RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MainActivity.kt
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MainActivity.kt
@@ -2,11 +2,32 @@ package edu.rosehulman.menga.recipetracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button_search.setOnClickListener {
+            Snackbar.make(activity_main, "search clicked", Snackbar.LENGTH_SHORT)
+        }
+        button_search.setOnLongClickListener {
+            Toast.makeText(this, "search clicked", Toast.LENGTH_SHORT)
+            button_search.text = "recognized"
+            true
+        }
+        button_my_favorites.setOnClickListener {
+            Snackbar.make(activity_main, "my_favorites clicked", Snackbar.LENGTH_SHORT)
+        }
+        button_my_recipes.setOnClickListener {
+            Snackbar.make(activity_main, "my_recipes clicked", Snackbar.LENGTH_SHORT)
+        }
+        button_popular.setOnClickListener {
+            Snackbar.make(activity_main, "popular clicked", Snackbar.LENGTH_SHORT)
+        }
     }
 }
