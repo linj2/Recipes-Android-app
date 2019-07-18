7fa52ad0980f7865e1a93de758b5c673a778c1e1
Andrew Meng <menga@rose-hulman.edu>
2019-07-18 15:00:37 -0400

    Added basic adapter function - can add rows to "my recipes"

--- RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MeFragment.kt
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MeFragment.kt
@@ -2,10 +2,16 @@ package edu.rosehulman.menga.recipetracker
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.*
+import kotlinx.android.synthetic.main.dialog_edit_recipe.view.*
 import kotlinx.android.synthetic.main.fragment_me.view.*
 
 class MeFragment: Fragment() {
+
+    lateinit var adapter: RecipeAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_me,container,false)
@@ -18,6 +24,35 @@ class MeFragment: Fragment() {
             }
             ft.commit()
         }
+        adapter = RecipeAdapter(context!!,  "")
+        view.recycler_view.adapter = adapter
+        view.recycler_view.layoutManager = LinearLayoutManager(context)
+        view.recycler_view.setHasFixedSize(true)
+
+        view.button_add_recipe.setOnClickListener {
+            //TODO: get a new Recipe and add it in the adapter
+            val builder = AlertDialog.Builder(context!!)
+            // Set options
+            val view = LayoutInflater.from(context).inflate(R.layout.dialog_edit_recipe, null, false)
+            builder.setView(view)
+            builder.setPositiveButton(android.R.string.ok) {_, _ ->
+                val title = view.recipe_edit_title.text.toString()
+                val ingredients = view.ingredients_edit_text.text.toString()
+                val instructions = view.instructions_edit_text.text.toString()
+                val ingredientList = toIngredientList(ingredients)
+                val recipe = Recipe(title, ingredientList, instructions)
+                adapter.add(recipe)
+            }
+            builder.setNegativeButton(android.R.string.cancel, null)
+            builder.create().show()
+        }
         return view
+    }
+
+    //TODO: write method to turn a string into a list of strings using ', ' or some sort of delimiter
+    fun toIngredientList(str: String): ArrayList<String> {
+        var list = ArrayList<String>()
+        list.add(str)
+        return list
     }
 }
