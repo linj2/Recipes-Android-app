207c9986b62eba6a33995cd0cf90b18f611774a7
Jing Lin <linj2@rose-hulman.edu>
2019-07-29 13:25:52 -0400

    delete something I have done that is not necessary

--- RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/Comment.kt
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/Comment.kt
@@ -2,32 +2,8 @@ package edu.rosehulman.menga.recipetracker
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Comment(var content: String?, val user: String?,val uid:String?):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(content)
-        parcel.writeString(user)
-        parcel.writeString(uid)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Comment> {
-        override fun createFromParcel(parcel: Parcel): Comment {
-            return Comment(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Comment?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
--- RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/Recipe.kt
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/Recipe.kt
@@ -26,37 +26,4 @@ data class Recipe(var title: String = "",
             return pic
         }
     }
-    /*
-    constructor(parcel: Parcel) : this (
-        parcel.readString(),
-        parcel.readArrayList(null) as ArrayList<String>,
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(instructions)
-        //parcel.writeStringArray(ingredients.toTypedArray())
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Recipe> {
-        const val CREATION_KEY = "creation"
-        fun fromSnapshot(snapshot: DocumentSnapshot):Recipe{
-            val pic = snapshot.toObject(Recipe::class.java)!!
-            pic.id = snapshot.id
-            return pic
-        }
-
-        override fun createFromParcel(source: Parcel): Recipe {
-            return Recipe(source)
-        }
-
-        override fun newArray(size: Int): Array<Recipe?> {
-            return arrayOfNulls(size)
-        }
-    }*/
 }
