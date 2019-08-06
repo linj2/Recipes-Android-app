72b65fa6e734c091f4c2052b977fcfea82e37757
Andrew Meng <menga@rose-hulman.edu>
2019-08-05 23:27:50 -0400

    A start to adding pictures of recipes
    Picture is not stored, but a decent start towards choosing a picture

---
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/AddToStorageTask.kt
@@ -0,0 +1,10 @@
+package edu.rosehulman.menga.recipetracker
+
+import android.graphics.Bitmap
+import com.google.firebase.firestore.CollectionReference
+
+class AddToStorageTask {
+    fun execute(urlString: String, id: String, bitmap: Bitmap, recipeRef: CollectionReference) {
+        val recipe = Recipe()
+    }
+}
--- RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MeFragment.kt
+++ RecipeTracker/app/src/main/java/edu/rosehulman/menga/recipetracker/MeFragment.kt
@@ -1,16 +1,39 @@
 package edu.rosehulman.menga.recipetracker
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.*
 import android.widget.EditText
 import android.widget.RelativeLayout
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.dialog_edit_recipe.view.*
 import kotlinx.android.synthetic.main.fragment_me.view.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
+private const val RC_TAKE_PICTURE = 1
+private const val RC_CHOOSE_PICTURE = 2
 
 class MeFragment: Fragment() {
 
@@ -18,6 +41,16 @@ class MeFragment: Fragment() {
     private var uid: String? = null
     lateinit var adapter: RecipeAdapter
     private var listener: RecipeAdapter.OnRecipeSelectedListener? = null
+    private var currentPhotoPath = ""
+
+    val thumbnailRef = FirebaseFirestore
+        .getInstance()
+        .collection(Constants.RECIPES_PATH)
+
+    private val storageRef =
+        FirebaseStorage
+            .getInstance()
+            .reference.child("images")
 
     companion object {
         fun newInstance(uid: String) =
@@ -76,6 +109,9 @@ class MeFragment: Fragment() {
             builder.setPositiveButton(android.R.string.ok, null)
             builder.setNeutralButton("+", null)
             builder.setNegativeButton(android.R.string.cancel, null)
+            view.recipe_image.setOnClickListener {
+                showPictureDialog()
+            }
             val titleEditText = view.findViewById<EditText>(R.id.edit_title)
             val dialog = builder.create()
             var lastID = R.id.ingredients_edit_text
@@ -117,5 +153,167 @@ class MeFragment: Fragment() {
             dialog.show()
         }
         return view
+    }
+
+    private fun showPictureDialog() {
+        val builder = AlertDialog.Builder(context!!)
+        builder.setTitle("Choose a photo source")
+        builder.setMessage("Would you like to take a new picture?\nOr choose an existing one?")
+        builder.setPositiveButton("Take Picture") { _, _ ->
+            launchCameraIntent()
+        }
+
+        builder.setNegativeButton("Choose Picture") { _, _ ->
+            launchChooseIntent()
+        }
+        builder.create().show()
+    }
+
+    // Everything camera- and storage-related is from
+    // https://developer.android.com/training/camera/photobasics
+    private fun launchCameraIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    // authority declared in manifest
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        context!!,
+                        "edu.rosehulman.catchandkit",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, RC_TAKE_PICTURE)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val storageDir: File = activity!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun launchChooseIntent() {
+        // https://developer.android.com/guide/topics/providers/document-provider
+        val choosePictureIntent = Intent(
+            Intent.ACTION_OPEN_DOCUMENT,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        choosePictureIntent.addCategory(Intent.CATEGORY_OPENABLE)
+        choosePictureIntent.type = "image/*"
+        if (choosePictureIntent.resolveActivity(context!!.packageManager) != null) {
+            startActivityForResult(choosePictureIntent, RC_CHOOSE_PICTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                RC_TAKE_PICTURE -> {
+                    sendCameraPhotoToAdapter()
+                }
+                RC_CHOOSE_PICTURE -> {
+                    sendGalleryPhotoToAdapter(data)
+                }
+            }
+        }
+    }
+
+    private fun sendCameraPhotoToAdapter() {
+        addPhotoToGallery()
+        Log.d(Constants.TAG, "Sending to adapter this photo: $currentPhotoPath")
+        //adapter.add(currentPhotoPath)
+    }
+
+    private fun addPhotoToGallery() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            activity!!.sendBroadcast(mediaScanIntent)
+        }
+    }
+
+    private fun sendGalleryPhotoToAdapter(data: Intent?) {
+        if (data != null && data.data != null) {
+            val location = data.data!!.toString()
+            //adapter.add(location)
+        }
+    }
+
+    inner class ImageRescaleTask(val localPath: String) : AsyncTask<Void, Void, Bitmap>() {
+        override fun doInBackground(vararg p0: Void?): Bitmap? {
+            // Reduces length and width by a factor (currently 2).
+            val ratio = 2
+            return BitmapUtils.rotateAndScaleByRatio(context!!, localPath, ratio)
+        }
+
+        override fun onPostExecute(bitmap: Bitmap?) {
+            // TODO: Write and call a new storageAdd() method with the path and bitmap
+            // that uses Firebase storage.
+            // https://firebase.google.com/docs/storage/android/upload-files
+            storageAdd(localPath, bitmap)
+        }
+
+        fun storageAdd(path: String, bitmap: Bitmap?): String? {
+            val baos = ByteArrayOutputStream()
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val bytes = baos.toByteArray()
+            val id = Math.abs(Random.nextLong()).toString()
+            var uploadTask = storageRef.child(id).putBytes(bytes)
+            uploadTask.addOnFailureListener {
+                // Handle unsuccessful uploads
+            }.addOnSuccessListener {
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+            }
+
+            uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation storageRef.child(id).downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+
+                    val task2: AddToStorageTask = AddToStorageTask()
+//                        when (listener!!.getTaskType()) {
+//                        MLKitTaskType.LABEL -> MLKitLabelTask()
+//                        MLKitTaskType.OCR -> MLKitOcrTask()
+//                        MLKitTaskType.OCR_CLOUD -> MLKitCloudOcrTask()
+//                        MLKitTaskType.BARCODE -> MLKitBarcodeTask()
+//                        MLKitTaskType.LABEL_CLOUD -> MLKitCloudLabelTask()
+//                        null -> MLKitLabelTask()
+//                    }
+                    task2.execute(downloadUri.toString(), id, bitmap!!, thumbnailRef)
+
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+            return null
+        }
     }
 }
